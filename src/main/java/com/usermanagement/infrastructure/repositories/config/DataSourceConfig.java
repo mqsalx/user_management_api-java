c555de6a3a2cac5ea4b6795250588fb4ae5877cd
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-04-08 20:36:14 -0300

    feat (Configuration): add DataSourceConfig for database connection management

---
+++ src/main/java/com/usermanagement/infrastructure/repositories/config/DataSourceConfig.java
@@ -0,0 +1,37 @@
+// /src/main/java/com/usermanagement/infrastructure/repositories/config/DataSourceConfig.java
+
+package com.usermanagement.infrastructure.repositories.config;
+
+import com.usermanagement.core.config.EnvConfig;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+  private final EnvConfig envConfig;
+
+  @Autowired
+  public DataSourceConfig(EnvConfig envConfig) {
+    this.envConfig = envConfig;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+
+    String url = envConfig.getDatasourceUrl();
+    String username = envConfig.getDatasourceUsername();
+    String password = envConfig.getDatasourcePassword();
+    String driverClassName = envConfig.getDatasourceDriver();
+
+    return DataSourceBuilder.create()
+        .url(url)
+        .username(username)
+        .password(password)
+        .driverClassName(driverClassName)
+        .build();
+  }
+}
