8d38b04c7adc1706b49fa38d759ead131df451a4
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-05-06 21:16:42 -0300

    feat: update UserRoutes to include DELETE route for user deletion

--- src/main/java/com/usermanagement/api/routes/user/UserRoutes.java
+++ src/main/java/com/usermanagement/api/routes/user/UserRoutes.java
@@ -2,15 +2,16 @@
 
 package com.usermanagement.api.routes.user;
 
+import org.springframework.stereotype.Component;
+import static org.springframework.web.servlet.function.RequestPredicates.DELETE;
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
 import static org.springframework.web.servlet.function.RequestPredicates.PATCH;
 import static org.springframework.web.servlet.function.RequestPredicates.POST;
+import org.springframework.web.servlet.function.RouterFunction;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
+import org.springframework.web.servlet.function.ServerResponse;
 
 import com.usermanagement.api.controllers.user.UserController;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerResponse;
 
 @Component
 public class UserRoutes {
@@ -24,6 +25,7 @@ public class UserRoutes {
     public RouterFunction<ServerResponse> userRouter() {
         return route(POST("/users"), controller::createUserController)
                 .andRoute(PATCH("/users/{id}"), controller::updateUserController)
-                .andRoute(GET("/users"), controller::getAllUsersController);
+                .andRoute(GET("/users"), controller::getAllUsersController)
+                .andRoute(DELETE("/users/{id}"), controller::deleteUserController);
     }
 }
