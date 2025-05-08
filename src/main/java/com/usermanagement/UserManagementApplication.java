c3622a511d5006433d7abbd700804d82241d17c8
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2025-05-08 20:09:13 -0300

    feat(logging): enhance logging configuration by loading settings from Dotenv

--- src/main/java/com/usermanagement/UserManagementApplication.java
+++ src/main/java/com/usermanagement/UserManagementApplication.java
@@ -11,6 +11,15 @@ import org.springframework.context.annotation.Bean;
 public class UserManagementApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Logging configuration
+        System.setProperty("logging.level.root", dotenv.get("API_LOGGING_LEVEL_ROOT", "INFO"));
+        System.setProperty("logging.level.org.springframework",
+                dotenv.get("API_LOGGING_LEVEL_ORG_SPRINGFRAMEWORK", "INFO"));
+        System.setProperty("logging.level.org.hibernate", dotenv.get("API_LOGGING_LEVEL_ORG_HIBERNATE", "INFO"));
+
         SpringApplication.run(UserManagementApplication.class, args);
     }
 
